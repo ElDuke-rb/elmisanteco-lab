e2a40470a17a31df1034694495dd863d4c49b3e4
ElMisanteco <elmisanteco@yahoo.com>
2025-06-17 18:41:57 -0600

    Se añadió Python: Texto_Colorido

---
+++ Python/8-Texto_Colorido/main.py
@@ -0,0 +1,9 @@
+from colorama import Fore
+
+print(Fore.GREEN + "¡Hola Mundo!")
+print(Fore.BLUE + "Hello World!")
+print(Fore.YELLOW + "Bonjour le Monde!")
+print(Fore.RED + "Ciao Mondo!")
+print(Fore.CYAN + "Olá Mundo!")
+print(Fore.MAGENTA + "ElMisanteco 😃")
+
